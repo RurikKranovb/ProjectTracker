2ee0c40d8e8062db0c8b51cfe006313c9c64b2c2
bobrov.da <dmr.bobrov@yandex.ru>
2025-08-18 05:51:13 +0300

    Добавьте файлы проекта.

---
+++ ProjectTracker/Program.cs
@@ -0,0 +1,57 @@
+namespace ProjectTracker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddControllersWithViews();
+
+           builder.Services.AddSession();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseSession();
+            app.UseRouting();
+
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapRazorPages()
+                .WithStaticAssets();
+
+            using (var scope = app.Services.CreateScope())
+            {
+
+            }
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.Run();
+        }
+    }
+    }
+}
