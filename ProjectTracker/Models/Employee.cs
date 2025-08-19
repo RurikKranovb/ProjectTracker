0cedd6b08261091c3939fbe2367cf2e27c524ae3
bobrov.da <dmr.bobrov@yandex.ru>
2025-08-19 20:23:20 +0300

    Add model employee

---
+++ ProjectTracker/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectTracker.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+    }
+}
