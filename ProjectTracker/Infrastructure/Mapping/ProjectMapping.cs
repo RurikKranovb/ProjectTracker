2333a2cbfbf89528ea388cd0a1302b361a414375
bobrov.da <dmr.bobrov@yandex.ru>
2025-08-25 19:00:33 +0300

    Add Mapping

---
+++ ProjectTracker/Infrastructure/Mapping/ProjectMapping.cs
@@ -0,0 +1,17 @@
+﻿using ProjectTracker.Domain.Entities;
+using ProjectTracker.ViewModels;
+
+namespace ProjectTracker.Infrastructure.Mapping
+{
+    public static class ProjectMapping
+    {
+        public static ProjectViewModel ToView(this Project p) => new ProjectViewModel()
+        {
+            Id = p.Id,
+            Name = p.Name,
+            Description = p.Description,
+        };
+
+        public static IEnumerable<ProjectViewModel> ToView(this IEnumerable<Project> p) => p.Select(ToView);
+    }
+}
---
+++ ProjectTracker/Infrastructure/Mapping/TaskMapping.cs
@@ -0,0 +1,18 @@
+﻿using ProjectTracker.Domain.Entities;
+using ProjectTracker.ViewModels;
+
+namespace ProjectTracker.Infrastructure.Mapping
+{
+    public static class TaskMapping
+    {
+        public static TaskViewModel ToView(this ProjectTask p) => new TaskViewModel()
+        {
+            Id = p.Id,
+            Name = p.Name,
+            Description = p.Description,
+            ProjectId = p.ProjectId
+        };
+
+        public static IEnumerable<TaskViewModel> ToView(this IEnumerable<ProjectTask> p) => p.Select(ToView);
+    }
+}
