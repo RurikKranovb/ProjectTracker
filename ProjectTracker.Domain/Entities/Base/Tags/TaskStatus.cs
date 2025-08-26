6dbc515bc3ce36dd18da5ca63481a310650e2ec1
bobrov.da <dmr.bobrov@yandex.ru>
2025-08-26 20:23:24 +0300

    refactoring

--- ProjectTracker.Domain/Entities/Base/Tags/TaskStatus.cs
+++ ProjectTracker.Domain/Entities/Base/Tags/TaskStatus.cs
@@ -1,9 +1,14 @@
-﻿namespace ProjectTracker.Domain.Entities.Base.Tags
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectTracker.Domain.Entities.Base.Tags
 {
     public enum TaskStatus
     {
+        [Display(Name = "Не начато")]
         NotStarted,
+        [Display(Name = "В процессе")]
         InProgress,
+        [Display(Name = "Выполнено")]
         Completed
     }
 }
